a23f44beea0af5edfda09deff060527ec6916ada
BenYoo-ben <neverknown0909@gmail.com>
2021-03-22 18:11:00 +0900

    cam test

--- client/src/Main.java
+++ client/src/Main.java
@@ -3,7 +3,6 @@ import javax.swing.JFrame;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -43,7 +42,21 @@ public class Main {
 		 * 
 		 */
 	
-		VideoHandler vh = new VideoHandler();
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setFPSDisplayed(true);
+		panel.setDisplayDebugInfo(true);
+		panel.setImageSizeDisplayed(true);
+		panel.setMirrored(true);
+
+		JFrame window = new JFrame("Test webcam panel");
+		window.add(panel);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
 	}
 
 }
