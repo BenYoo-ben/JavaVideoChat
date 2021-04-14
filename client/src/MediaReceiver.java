776f266353aca7c78b6c0073f08419079b008012
BenYoo-yoo <neverknown0909@gmail.com>
2021-04-15 02:11:09 +0900

    double pass comm

--- client/src/MediaReceiver.java
+++ client/src/MediaReceiver.java
@@ -21,18 +21,18 @@ class MediaReceiver extends Thread
 			}catch(Exception e) {
 					System.out.println("exc");
 			}
-		String[] input;
+		String input;
 		while(true)
 		{
-			input = parseRecv(th.Receive());
+			input = new String(th.Receive());
 			
-			switch(Integer.parseInt(input[0]))
+			switch(Integer.parseInt(input))
 			{
 			case Global.OP.VIDEO_DATA:
-				//jp.getImage(input[1].getBytes());
+				
 				try {
-				byte[] tmp = Files.readAllBytes(Paths.get("022.jpg"));
-				jp.getImage(tmp);
+				
+				jp.getImage(th.Receive());
 				jp.repaint();
 				}catch(Exception e) {
 						System.out.println("exc");
@@ -46,19 +46,6 @@ class MediaReceiver extends Thread
 		
 	}
 	
-	protected String[] parseRecv(byte[] input) {
-		if (input.length <= 0)
-			return null;
 
-		//parse request data(in [OP_TYPE]#[Data])
-		String[] parsed = new String[2];
-
-		String whole = new String(input);
-
-		parsed = whole.split("[#]");
-
-		return parsed;
-
-	}
 	
 }
--- client/src/MediaSender.java
+++ client/src/MediaSender.java
@@ -14,19 +14,19 @@ class MediaSender extends Thread
 	
 	public void run()
 	{
-		byte[] header = new String(Global.OP.VIDEO_DATA+"#").getBytes();
+		byte[] header = new String(Global.OP.VIDEO_DATA).getBytes();
 		while(true)
 		{
 			
-			System.out.println("Preparing Send");
-			byte[] data =  vh.CaptureToBytes();
-			byte[] whole_data = new byte[header.length + data.length];
-			System.arraycopy(header, 0, whole_data, 0, header.length); 
-		    System.arraycopy(data, 0, whole_data, header.length, data.length); 
-			System.out.println("Sending : "+new String(whole_data));
-			if(whole_data==null)
-				System.out.println("Trying to send null?\n\n");
+			System.out.println("Sending Header...");
 			th.Send(whole_data);
+			System.out.println("Sending Header Complete!");
+			
+			byte[] data =  vh.CaptureToBytes();
+			System.out.println("Sending Data...");
+			th.Send(whole_data);
+			System.out.println("Sending Data Complete!");
+			
 			System.out.println("Sent! "+ send_count);
 			send_count++;
 		}
